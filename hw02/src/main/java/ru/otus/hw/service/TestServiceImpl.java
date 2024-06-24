ea849eb2f7df66d10456e9af8f0a9f736d77778e
Viktoriya.Demidova <Viktoriya.Demidova@norbit.ru>
2024-06-24 17:52:21 +0300

    add annotations, tests

---
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question: questions) {
+            var isAnswerValid = false;
+            ioService.printLine(question.text());
+            if (question.answers() != null) {
+                ioService.printFormattedLine(question.answers().stream()
+                        .map(Answer::text).collect(Collectors.joining("\n")));
+                int answer = ioService.readIntForRange(1, 3, "Введите цифру от 1 до 3");
+                isAnswerValid = question.answers().get(answer - 1).isCorrect();
+                testResult.applyAnswer(question, isAnswerValid);
+            }
+        }
+        return testResult;
+    }
+}
---
+++ hw02/src/test/java/dao/CsvQuestionDaoTest.java
@@ -0,0 +1,63 @@
+package dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.otus.hw.Application;
+import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = Application.class)
+@TestPropertySource(locations = "classpath:/application-test.properties")
+@DisplayName("Метод findAll() должен ")
+public class CsvQuestionDaoTest {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private AppProperties appProperties;
+
+    @Test
+    @DisplayName(" не бросать исключение если файл найден")
+    public void shouldNotThrowQuestionReadExceptionIfQuestionsFileExists() {
+        assertThatCode(() -> questionDao.findAll()).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName(" бросать исключение если файл не найден")
+    public void shouldThrowQuestionReadExceptionIfQuestionsFileNotExists() {
+        appProperties.setTestFileName("questions_not_exist.csv");
+        assertThatThrownBy(() -> questionDao.findAll()).isInstanceOf(QuestionReadException.class);
+        appProperties.setTestFileName("questions_test.csv");
+    }
+
+    @Test
+    @DisplayName(" вернуть корректный список вопросов")
+    public void shouldReturnCorrectQuestionsList() {
+        Question question = getTestData();
+        assertThat(questionDao.findAll()).isNotEmpty().containsExactly(question);
+    }
+
+    private static Question getTestData() {
+        List<Answer> answers = new ArrayList<>();
+        answers.add(new Answer("1. yes", true));
+        answers.add(new Answer("2. no", false));
+        return new Question("Read correctly?", answers);
+    }
+}
