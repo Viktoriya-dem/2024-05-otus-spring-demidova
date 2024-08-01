52ebdd748550daa8dad9f61825fa31f776837755
Viktoriya.Demidova <Viktoriya.Demidova@norbit.ru>
2024-08-01 11:36:37 +0300

    hw07 change repo on spring-data

---
+++ hw07/src/main/java/ru/otus/hw/commands/CommentCommands.java
@@ -0,0 +1,53 @@
+package ru.otus.hw.commands;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import ru.otus.hw.converters.CommentConverter;
+import ru.otus.hw.services.CommentService;
+
+import java.util.stream.Collectors;
+
+@SuppressWarnings({"SpellCheckingInspection", "unused"})
+@RequiredArgsConstructor
+@ShellComponent
+public class CommentCommands {
+
+    private final CommentService commentService;
+
+    private final CommentConverter commentConverter;
+
+    @ShellMethod(value = "Find all book comments", key = "abc")
+    public String findAllBookComments(long bookId) {
+        return commentService.findAllByBookId(bookId).stream()
+                .map(commentConverter::commentToString)
+                .collect(Collectors.joining("," + System.lineSeparator()));
+    }
+
+    @ShellMethod(value = "Find comment by id", key = "cbid")
+    public String findCommentById(long id) {
+        return commentService.findById(id)
+                .map(commentConverter::commentToString)
+                .orElse("Comment with id %d not found".formatted(id));
+    }
+
+    // cins newComment 1
+    @ShellMethod(value = "Insert comment", key = "cins")
+    public String insertComment(String text, long bookId) {
+        var savedComment = commentService.insert(text, bookId);
+        return commentConverter.commentToString(savedComment);
+    }
+
+    // bupd 4 editedComment 2
+    @ShellMethod(value = "Update comment", key = "cupd")
+    public String updateComment(long id, String text) {
+        var savedComment = commentService.update(id, text);
+        return commentConverter.commentToString(savedComment);
+    }
+
+    // cdel 4
+    @ShellMethod(value = "Delete comment by id", key = "cdel")
+    public void deleteComment(long id) {
+        commentService.deleteById(id);
+    }
+}
---
+++ hw07/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
@@ -0,0 +1,112 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Сервис работы с книгами должен ")
+@DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Import({BookServiceImpl.class})
+public class BookServiceImplTest {
+
+    @Autowired
+    private BookService bookService;
+
+    private static Book getBookData() {
+        return new Book(1L, "BookTitle_1", getAuthorData(), getGenreData());
+    }
+
+    private static Book getNewBookData() {
+        return new Book(4L, "NewBookTitle_1", getAuthorData(), getGenreData());
+    }
+
+    private static Author getAuthorData() {
+        return new Author(1L, "Author_1");
+    }
+
+    private static List<Genre> getGenreData() {
+        return List.of(new Genre(1L, "Genre_1"), new Genre(2L, "Genre_2"));
+    }
+
+    @Test
+    @DisplayName(" вернуть корректную книгу по id")
+    public void shouldReturnCorrectBookById() {
+        Optional<Book> actualBook = bookService.findById(1L);
+        var expectedBook = getBookData();
+
+        assertThat(actualBook).isPresent();
+
+        assertThat(actualBook.get()).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison().isEqualTo(expectedBook);
+    }
+
+    @DisplayName("должен загружать список всех книг")
+    @Test
+    void shouldReturnCorrectBooksList() {
+        var actualBooks = bookService.findAll();
+
+        assertThat(actualBooks).isNotEmpty()
+                .hasSize(3)
+                .hasOnlyElementsOfType(Book.class);
+    }
+
+    @DisplayName("должен сохранять новую книгу")
+    @Test
+    void shouldSaveNewBook() {
+        var expectedBook = getNewBookData();
+        var actualBook = bookService.insert(expectedBook.getTitle(), expectedBook.getAuthor().getId(),
+                expectedBook.getGenres().stream().map(Genre::getId).collect(Collectors.toSet()));
+        assertThat(actualBook).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison().isEqualTo(expectedBook);
+    }
+
+    @DisplayName("должен сохранять измененную книгу")
+    @Test
+    void shouldSaveUpdatedBook() {
+        var expectedBook = getBookData();
+        expectedBook.setTitle("Change_Title_1");
+
+        assertThat(bookService.findById(expectedBook.getId()))
+                .isPresent();
+
+        assertThat(bookService.findById(expectedBook.getId()).get().getTitle()).isNotEqualTo(expectedBook.getTitle());
+
+        var actualBook = bookService.update(1L, expectedBook.getTitle(), expectedBook.getAuthor().getId(),
+                expectedBook.getGenres().stream().map(Genre::getId).collect(Collectors.toSet()));
+        assertThat(actualBook).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison().isEqualTo(expectedBook);
+
+        assertThat(bookService.findById(actualBook.getId()))
+                .isPresent();
+    }
+
+    @DisplayName("должен удалять книгу по id ")
+    @Test
+    void shouldDeleteBook() {
+        assertThat(bookService.findById(1L)).isPresent();
+        bookService.deleteById(1L);
+
+        var actualBook = bookService.findById(1L);
+        assertThat(actualBook).isEmpty();
+    }
+
+}
